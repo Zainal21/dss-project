c819cfe4f610cd3ae614d660867a9a538fa61d3f
Zainal21 <Zainalarifin080718@gmail.com>
2023-06-23 07:20:13 +0700

    feat : auth service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,7 +6,10 @@ import {
   HttpCode,
   UsePipes,
   ValidationPipe,
+  BadRequestException,
+  Res,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
@@ -18,14 +21,32 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   @Post('register')
   @UsePipes(new ValidationPipe({ transform: true }))
-  create(@Body() RegisterDto: RegisterDto) {
-    return this.authService.signUp(RegisterDto);
+  async create(@Body() RegisterDto: RegisterDto, @Res() res: Response) {
+    const register = await this.authService.signUp(RegisterDto);
+
+    if (!register)
+      throw new BadRequestException('Invalid Parameter when register user');
+
+    res.status(HttpStatus.OK).json({
+      statusCode: register.statusCode,
+      message: register.message,
+      data: register.data,
+    });
   }
 
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UsePipes(new ValidationPipe({ transform: true }))
-  login(@Body() LoginDto: LoginDto) {
-    return this.authService.login(LoginDto);
+  async login(@Body() LoginDto: LoginDto, @Res() res: Response) {
+    const authenticated = await this.authService.login(LoginDto);
+
+    if (!authenticated || authenticated?.statusCode > 200)
+      throw new BadRequestException('Invalid login, please try again');
+
+    res.status(HttpStatus.OK).json({
+      statusCode: authenticated.statusCode,
+      message: authenticated.message,
+      data: authenticated.data,
+    });
   }
 }
--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,21 +1,28 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Delete,
   Get,
   HttpCode,
   HttpStatus,
+  NotFoundException,
   Param,
   Patch,
   Post,
+  Res,
   UseGuards,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { CategoriesService } from './categories.service';
 import { AuthGuard } from 'src/shared/security/auth.guard';
 import { CategoryDto } from './categories.dto';
 
 @UseGuards(AuthGuard)
 @Controller('categories')
+@UsePipes(new ValidationPipe({ transform: true }))
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
@@ -27,14 +34,29 @@ export class CategoriesController {
 
   @HttpCode(HttpStatus.OK)
   @Get(':id')
-  async findById(@Param('id') id) {
-    return await this.categoriesService.getCategoryById(id);
+  async findById(@Param('id') id, @Res() res: Response) {
+    const category = await this.categoriesService.getCategoryById(id);
+    if (!category) throw new NotFoundException('Catgeory not found');
+
+    res.status(HttpStatus.OK).json({
+      statusCode: category.statusCode,
+      message: category.message,
+      data: category.data,
+    });
   }
 
   @HttpCode(HttpStatus.OK)
   @Post()
-  async create(@Body() categoryDto: CategoryDto) {
-    return await this.categoriesService.createCategory(categoryDto);
+  async create(@Body() categoryDto: CategoryDto, @Res() res: Response) {
+    const category = await this.categoriesService.createCategory(categoryDto);
+
+    if (!category) throw new BadRequestException('Invalid Parameter');
+
+    res.status(HttpStatus.OK).json({
+      statusCode: category.statusCode,
+      message: category.message,
+      data: category.data,
+    });
   }
 
   @HttpCode(HttpStatus.OK)
